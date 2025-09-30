82ebfacd38a217d2c9603b6ac0c4bf88baa951ee
Anusha9859 <marisearuna@gmail.com>
2025-09-30 21:04:40 +0000

    Updated chat app with new changes

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -32,7 +32,7 @@ const Homepage = () => {
       >
       <Flex justifyContent="center" p={3} bg="white" w="100%" m="40px 0 15px 0" borderRadius="lg" borderWidth="1px">
           <Text fontSize="4xl" fontFamily="Work sans">
-            Talk-A-Tive
+            Let's Chat
           </Text>
       </Flex>
       </Box>
